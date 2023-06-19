150be65d2e047837f4bffd5d9e300e5a097eaa9f
Brandon Ray <bray387@outlook.com>
2023-06-19 15:05:04 -0500

    adds a couple of my recent projects to the site and fixes a few issues with the main page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,35 +5,36 @@ import Me from './images/Me.png'
 
 const HomePage = () => {
     return (
-    <>
-        <section className="home">
-            <div className='cherry-blossum-img'>
-                <img src={Img1Portfolio} alt="My Image" height="550px" width="800px" style={{ borderRadius: '30px' }} />         
-            </div>
-            <div>
-                <div className="self-image-grey-container">
-                    <p className="about-me-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, tenetur enim ratione neque maxime ipsam pariatur totam laudantium quos quaerat? </p>
-                </div>           
-                <div className="self-image">
-                    <img src={Me} alt="My Self Image" />     
+        <>
+            <section className="#home" id='#'>
+                <div className='cherry-blossum-img'>
+                    <img src={Img1Portfolio} alt="My Image" height="550px" width="800px" style={{ borderRadius: '30px' }} />         
                 </div>
-            </div>
-            <div className='full-stack-container'>
-                <div className="intro-name">
-                    <h1>Hi, I'm <span style={{ color: '#ea1538' }}> Brandon </span></h1>
+                <div>
+                    <div className="self-image-grey-container">
+                        <p className="about-me-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, tenetur enim ratione neque maxime ipsam pariatur totam laudantium quos quaerat? </p>
+                    </div>           
+                    <div className="self-image">
+                        <img src={Me} alt="My Self Image" />     
+                    </div>
                 </div>
-                <div className="full-stack-web-developer">
-                    <br /> 
-                    <h2>Full Stack Web Developer</h2>
+                <div className='full-stack-container'>
+                    <div className="intro-name">
+                        <h1>Hi, I'm <span style={{ color: '#ea1538' }}> Brandon </span></h1>
+                    </div>
+                    <div className="full-stack-web-developer">
+                        <br /> 
+                        <h2>Full Stack Software Developer</h2>
+                    </div>
+                    <div className="short-motivation">
+                        <p>As a brand new software developer armed with the latest skills and knowledge, I am eager to embark on this thrilling journey as a software developer. I am confident in my ability to tackle complex coding challenges, collaborate with fellow developers, and contribute meaningfully to innovative projects. Ready to make my mark in the industry, I am motivated to continuously learn and grow as a professional software developer.
+                        </p> 
+                        <a href="#my-projects" className="button">Click Here To See My Work</a>
+                    </div>
                 </div>
-                <div className="short-motivation">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, impedit ea. Possimus aliquam error nostrum itaque nobis, pariatur minima exercitationem sequi suscipit quo ducimus quae porro, quia, ipsum quod molestias!
-                    </p> 
-                </div>
-            </div>
-        </section>
-    </>
-);
+            </section>
+        </>
+    );
 }
 
 export default HomePage;
